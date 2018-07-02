eb8c76296926a889e6bde93da87ca5bb5f9b16ae
Franz Lutzenberger <franz.lutzenberger@gmail.com>
2018-07-02 13:22:20 +0200

    beginning of a store

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,12 +5,18 @@ import { Grid, Paper } from '@material-ui/core'
 import Header from './Layouts/Header'
 import Footer from './Layouts/Footer'
 
+import { muscles, exercises } from '../store'
+
 const style = {
   Paper: { padding: 16, marginTop: 8, marginBottom: 8 },
   Grid: { maxWidth: 'none' }
 }
 
 class App extends React.Component {
+  states = {
+    exercises
+  }
+
   render() {
     return (
       <Fragment>
@@ -23,7 +29,7 @@ class App extends React.Component {
             <Paper style={style.Paper}>right pain</Paper>
           </Grid>
         </Grid>
-        <Footer />
+        <Footer muscles={muscles} />
       </Fragment>
     )
   }
--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import { Paper, Tabs, Tab } from '@material-ui/core'
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <Paper>
-        <Tabs value={0} indicatorColor="primary" textColor="primary" centered>
-          <Tab label="Item One" />
-          <Tab label="Item Two" />
-          <Tab label="Item Three" />
-        </Tabs>
-      </Paper>
-    )
-  }
-}
-
-export default Footer
+export default ({ muscles }) => (
+  <Paper>
+    <Tabs value={0} indicatorColor="primary" textColor="primary" centered>
+      <Tab key={'all'} label={'all'} />
+      {muscles.map(group => <Tab key={group} label={group} />)}
+    </Tabs>
+  </Paper>
+)
